d1c91bf7707b2e37871301a0317a5d6937db5b23
alex gnavi <alex.gnavi@gmail.com>
2023-02-09 15:49:28 +0100

    aggiunto file di setup

---
+++ setup-build.sh
@@ -0,0 +1,14 @@
+#install angular
+npm install angular
+
+#creare un nuovo progetto aprire cmd
+ng new mi app
+
+#usare il comando npm su code per installare gli altri package
+npm install ''
+
+#installare i package a livello globale
+
+npm install '' -g
+
+
