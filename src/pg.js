bff0d90a94eafdd12499ada9c4267406d8cc7c08
alex gnavi <alex.gnavi@gmail.com>
2023-02-22 09:47:03 +0100

    collegamento a mongo db

--- src/pg.js
+++ src/pg.js
@@ -1,43 +1,13 @@
-const pg = require('pg');
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
 
-const config = {
-    host: '<your-db-server-name>.postgres.database.azure.com',
-    // Do not hard code your username and password.
-    // Consider using Node environment variables.
-    user: '<alex.gnavi>',
-    password: '<itstelemed2123>',
-    database: '<evolus>',
-    port: 5432,
-    ssl: true
-};
-
-const client = new pg.Client(config);
-
-client.connect(err => {
+MongoClient.connect(url, function(err, db) {
+  if (err) throw err;
+  var dbo = db.db("dbmpalestra");//nome db
+  var query = { nome: "alex" };  //query
+  dbo.collection("progressi").find(query).toArray(function(err, result) {  //nome collection
     if (err) throw err;
-    else {
-        queryDatabase();
-    }
+    console.log(result);
+    db.close();
+  });
 });
-
-function queryDatabase() {
-    const query = `
-        DROP TABLE IF EXISTS inventory;
-        CREATE TABLE palestra(id serial PRIMARY KEY, name VARCHAR(50), allenamento VARCHAR(50));
-        INSERT INTO GYM (name, allenamento) VALUES ('alex', 'fullbody');
-        INSERT INTO GYM(name, allenamento) VALUES ('samuele', 'cardio');
-        INSERT INTO GYM (name, allenamento) VALUES ('diletta', ''metabolico);
-    `;
-
-    client
-        .query(query)
-        .then(() => {
-            console.log('Table created successfully!');
-            client.end(console.log('Closed client connection'));
-        })
-        .catch(err => console.log(err))
-        .then(() => {
-            console.log('Finished execution, exiting now');
-            process.exit();
-        });
-}
