78eb09ce74d18fc2e23f49c54cabb269d5007918
alex gnavi <alex.gnavi@gmail.com>
2023-03-01 11:11:09 +0100

    modifiche varie sul body html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { NgForm } from '@angular/forms';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
