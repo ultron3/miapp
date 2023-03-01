4244b6d1602264acf238389506156068622e3d5b
alex gnavi <alex.gnavi@gmail.com>
2023-03-01 12:14:02 +0100

    implementata classe email

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,3 +8,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'miapp';
 }
+
+
+export class Mail  {
+  later(email: string, after: number): boolean {
+      console.log(`Send email to ${email} in ${after} ms.`);
+      return true;
+  }
+  send(email: string,after:number): boolean {
+      console.log(`Sent email to ${email} after ${after} ms. `);
+      return true;
+  }
+  queue(email: string): boolean {
+      console.log(`Queue an email to ${email}.`);
+      return true;
+  }
+}
