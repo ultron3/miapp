bbad6aa8e0a235cb4b2c0a39abcde2c5886822a6
alex gnavi <alex.gnavi@gmail.com>
2023-03-07 16:25:20 +0100

    aggiunto componente action bar

---
+++ src/app/action.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { ActionBarService } from 'my-ui-library';
+
+@Component({
+  selector: 'my-component',
+  template: `
+    <h1>Il mio componente</h1>
+    <div>
+      <my-action-bar>
+        <button (click)="handleClick()">Pulsante dell'action bar</button>
+      </my-action-bar>
+    </div>
+  `,
+})
+export class MyComponent {
+  constructor(private actionBarService: ActionBarService) {}
+
+  handleClick() {
+    // gestisci il clic del pulsante dell'action bar qui
+  }
+}
