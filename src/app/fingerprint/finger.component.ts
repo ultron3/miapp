a09ed5cc8dc3e7d6bc0c76482c123f40d6b34e14
alex gnavi <alex.gnavi@gmail.com>
2023-03-08 15:27:53 +0100

    function get finger prtint

--- src/app/fingerprint/finger.component.ts
+++ src/app/fingerprint/finger.component.ts
@@ -14,5 +14,13 @@ const fpPromise = Fingerprintjs.load({
 })
 
 
+
 fpPromise
 .then((fp: { get: () => any }) => fp.get())
+
+
+function getFingerprint(obj: Record<string, unknown>): string {
+  const sortedEntries = Object.entries(obj).sort(([key1], [key2]) => key1.localeCompare(key2));
+  const concatenatedString = sortedEntries.map(([key, value]) => `${key}:${value}`).join(',');
+  return `${obj.constructor.name}(${concatenatedString})`;
+}
