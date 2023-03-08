be403f9d3f5733a171758be67cf1dfc2e15ffcdb
alex gnavi <alex.gnavi@gmail.com>
2023-03-08 14:51:03 +0100

    aggiunta cartella finger

--- src/app/fingerprint/finger.component.ts
+++ src/app/fingerprint/finger.component.ts
@@ -1,4 +1,11 @@
+import { Component } from '@angular/core'
 import {Fingerprintjs} from '@fingerprintjs/fingerprintjs-pro'
+
+
+Component({
+    templateUrl:'./finger.component.html',
+    styleUrls:['./finger.component.css']
+})
 
 const fpPromise = Fingerprintjs.load({
   apikey:'api-kei',
