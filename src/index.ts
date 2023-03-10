429a53bcb4b57196f198cffa67b5d7a98810bcdc
alex gnavi <alex.gnavi@gmail.com>
2023-03-10 11:12:01 +0100

    add nativescript

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,12 @@ Component({
   templateUrl:'./index.html',
   styleUrls:['./style.css']
 })
+
+
+//https://bobbyhadz.com/blog/all-in-programming
+
+function onTap(args: any, EventData: any) {
+  
+  throw new Error("Function not implemented.");
+}
+
