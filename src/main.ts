1a1b24bd05740a0551830eaeedefc2bed22bec8f
ultron3 <alex.gnavi@gmail.com>
2023-03-21 15:41:17 +0100

    risolti bug di produzione

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,13 @@ import { Component } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/component/app.module';
+import { enableProdMode } from '@angular/core';
+import { environment } from './enviroment.prod';
 
+if (environment.production) {
+  enableProdMode();
+}//La funzione enableProdMode() di Angular viene utilizzata per abilitare la modalità di produzione,
+// che fornisce prestazioni migliori e migliora l'esperienza dell'utente.
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));  //FIXME : da aggiornare
