2f134b6c294d0ded2d649a815af3d6659f7fb00e
ultron3 <alex.gnavi@gmail.com>
2023-04-05 13:58:34 +0200

    modifica token banca

--- src/app/pagamenti/paypol.token.ts
+++ src/app/pagamenti/paypol.token.ts
@@ -1,2 +1,13 @@
 //not implemented
 //fixme
+
+
+import { InjectionToken, Injector } from "@angular/core";
+
+const BASE_URL = new InjectionToken<string>('BaseUrl');
+const injector =
+    Injector.create({providers: [{provide: BASE_URL, useValue: 'https://www.unicredit.it/it/privati.html'}]});
+const url = injector.get(BASE_URL);
+// Note: since `BASE_URL` is `InjectionToken<string>`
+// `url` is correctly inferred to be `string`
+expect(url).toBe('https://www.unicredit.it/it/privati.html');
