febf8a1e450b46a2dfe1685e37e6fe0d919caf5d
ultron3 <alex.gnavi@gmail.com>
2023-04-07 16:56:01 +0200

    cambiato colore box

--- src/app/fingerprint/finger.post.ts
+++ src/app/fingerprint/finger.post.ts
@@ -1,14 +1,10 @@
 import {Fingerprintjs} from './finger.enviroments'
-
-
 const fpPromise = Fingerprintjs.load({
   apikey:'api-kei',
   region:'eu'
 
 
 })
-
-
 async function postfingerprint(){
   console.log("success")
   return
