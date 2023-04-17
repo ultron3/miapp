4d7171c41ff4bdbc3d1d4abe84de55e007df491c
ultron3 <alex.gnavi@gmail.com>
2023-04-17 12:13:56 +0200

    passo i parametri let apikey e let region

--- src/app/fingerprint/finger.post.ts
+++ src/app/fingerprint/finger.post.ts
@@ -1,10 +1,21 @@
 import {Fingerprintjs} from './finger.enviroments'
+import { ReflectiveInjector } from '@angular/core';
 const fpPromise = Fingerprintjs.load({
   apikey:'api-kei',
   region:'eu'
 
 
 })
+
+let injector = ReflectiveInjector.resolveAndCreate([
+  { provide: "api-kei", useValue: 'XYZ1234ABC' }
+]);
+
+let apikey = injector.get("api-kei");
+console.log(apikey); // "XYZ1234ABC"
+let region=injector.get('eu');
+console.log(region)
+
 async function postfingerprint(){
   console.log("success")
   return
