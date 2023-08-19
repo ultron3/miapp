d8115af7ea45ec5f602a3a165bbac02d75cea21e
ultron3 <alex.gnavi@gmail.com>
2023-08-19 11:22:43 +0200

    modifica sorgenti collegamneto mongodb

---
+++ MiAppDb/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string connectionString = "mongodb://localhost:27017";
+        MongoClient client = new MongoClient(connectionString);
+
+        IMongoDatabase database = client.GetDatabase("dbmpalestra");
+
+        IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("progressi"); 
+
+        var documents = collection.Find(new BsonDocument()).ToList();
+
+        foreach (var document in documents)
+        {
+            Console.WriteLine(document);
+        }
+    }
+}
