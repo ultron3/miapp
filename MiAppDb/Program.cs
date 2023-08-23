b1e5b7d15d4e11832d9f92728348fd675250891a
ultron3 <alex.gnavi@gmail.com>
2023-08-23 20:22:49 +0200

    visualizzo i dati in una tabella attraverso la libreria ConsoleTables

--- MiAppDb/Program.cs
+++ MiAppDb/Program.cs
@@ -1,24 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using MongoDB.Bson;
+using System.Threading.Tasks;
+using ConsoleTables;
+
 using MongoDB.Driver;
 //https://www.html.it/pag/68918/api-per-c/
 public class Program
 {
+    
     public static async Task Main(string[] args)
     {
+
         string connectionString = "mongodb://localhost:27017";
         MongoClient client = new MongoClient(connectionString);
 
         IMongoDatabase database = client.GetDatabase("dbmpalestra");
-        //stampo tutti i record del db
-        IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("progressi"); 
+        IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>("NewProgress");
 
-        var documents = collection.Find(new BsonDocument()).ToList();
+        var documents = await collection.Find(new BsonDocument()).ToListAsync();
+
+        var table = new ConsoleTable("Nome", "Username",  "Indirizzo","Somatotipo","Allenamento");
 
         foreach (var document in documents)
         {
-            Console.WriteLine(document);
+            string nome = document["nome"].AsString;
+           // string cognome= document["cognome"].AsString;
+           string username = document["username"].AsString;
+            string somatotipo = document["somatotipo"].AsString;
+            string indirizzo = document["indirizzo"].AsString;
+            
+            string allenamento = document["allenamento"].AsString;
+            //string somatotipo = document["somatotipo"].AsString;
+
+            // Aggiungi una riga alla tabella
+            table.AddRow(nome,username,  indirizzo,allenamento,somatotipo);
         }
+
+        // Stampa la tabella nella console
+        table.Write();
+
+
+
 
         //La creazione di un documento avviene mediante la classe BsonDocument
         //creo una nuova collection
@@ -36,7 +58,7 @@ public class Program
 
 
         };
-        var  collection1= database.GetCollection<BsonDocument>("NewProgress");
+        var collection1 = database.GetCollection<BsonDocument>("NewProgress");
         await collection1.InsertOneAsync(record);
 
         var records = new BsonDocument{
@@ -49,7 +71,7 @@ public class Program
             }}
         };
 
-        var  collection2= database.GetCollection<BsonDocument>("NewProgress");
+        var collection2 = database.GetCollection<BsonDocument>("NewProgress");
         await collection2.InsertOneAsync(records);
 
 
