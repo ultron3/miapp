434df8f046ada5c31fa857cc4443a956d249e248
ultron3 <alex.gnavi@gmail.com>
2023-08-23 20:30:54 +0200

    creata libreria

---
+++ MiAppDb/Class1.cs
@@ -0,0 +1,17 @@
+﻿namespace MiAppDb;
+
+public class Class1
+{
+
+    public string Nome{get;set;}
+    public string Cognome{get;set;}
+    public string Username{get;set;}
+
+    public string Pwd{get;set;}
+
+    public string Allenamento{get;set;}
+
+    public string Somatotipo{get;set;}
+
+
+}
