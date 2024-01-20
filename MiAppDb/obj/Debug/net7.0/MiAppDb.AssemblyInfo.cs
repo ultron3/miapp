b3991c958dbe8bbf814dddad4110d3bf9277f94a
ultron3 <alex.gnavi@gmail.com>
2024-01-20 13:47:45 +0100

    add backend db

--- MiAppDb/obj/Debug/net7.0/MiAppDb.AssemblyInfo.cs
+++ MiAppDb/obj/Debug/net7.0/MiAppDb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MiAppDb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df035108522dd12bab61d7bac1127b4c5caa7894")]
 [assembly: System.Reflection.AssemblyProductAttribute("MiAppDb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MiAppDb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
